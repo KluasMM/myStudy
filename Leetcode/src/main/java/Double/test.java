311a6b00f2ce1fd0e47b42f127d2f8486fac3193
Klaus <matian0720@foxmail.com>
2020-03-07 15:41:38 +0800

    branch task1

--- Leetcode/src/main/java/Double/test.java
+++ Leetcode/src/main/java/Double/test.java
@@ -8,6 +8,8 @@ public class test {
         int a = 1;
         int b = 3;
         int c = 4;
+        String task1 = "1";
+        System.out.println(task1);
         System.out.println(a+1);
     }
 }
