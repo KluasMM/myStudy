dea5dd68131a700556353ee0dc0cd1ac2e10ae6e
KluasMM <38516878+KluasMM@users.noreply.github.com>
2020-03-07 14:58:16 +0800

    Update test.java

--- Leetcode/src/main/java/Double/test.java
+++ Leetcode/src/main/java/Double/test.java
@@ -6,7 +6,7 @@ package Double;
 public class test {
     public static void main(String[] args) {
         int a = 1;
-        int b = 2;
+        int b = 3;
         System.out.println(a+1);
     }
 }
