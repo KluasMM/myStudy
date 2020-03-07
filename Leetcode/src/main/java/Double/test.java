9b37468a0db962e3faf3a9bb04039da3c3825dc3
Klaus <matian0720@foxmail.com>
2020-03-07 14:26:55 +0800

    first commit

---
+++ Leetcode/src/main/java/Double/test.java
@@ -0,0 +1,11 @@
+package Double;
+
+/**
+ * Created by Klaus on 2020/3/7.
+ */
+public class test {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
+    }
+}
